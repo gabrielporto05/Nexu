f4eed44ed2a113cfe42840e831100f5cc79ef388
chines05 <gpalmenara@gmail.com>
2025-10-02 18:46:55 -0300

    [Api] Feat: Implementação de mux para visualizacao de avatars

--- api/main.go
+++ api/main.go
@@ -13,17 +13,21 @@ import (
 func main() {
 	config.Loading()
 
-	router := router.Generate()
+	mux := http.NewServeMux()
+
+	mux.Handle("/uploads/", http.StripPrefix("/uploads/", http.FileServer(http.Dir("./uploads"))))
+
+	mux.Handle("/", router.Generate())
 
 	c := cors.New(cors.Options{
-		AllowedOrigins:   []string{"http://localhost:8081"},
+		AllowedOrigins:   []string{"http://192.168.6.121:8081"},
 		AllowedMethods:   []string{"GET", "POST", "PUT", "PATCH", "DELETE"},
 		AllowedHeaders:   []string{"Authorization", "Content-Type"},
 		AllowCredentials: true,
 	})
 
-	handler := c.Handler(router)
+	handler := c.Handler(mux)
 
-	fmt.Printf("Running server on http://localhost:%d\n", config.Port)
+	fmt.Printf("Running server on http://192.168.6.121:%d\n", config.Port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Port), handler))
 }
