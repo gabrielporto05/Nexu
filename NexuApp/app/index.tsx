61dfcadc2420420c7ddfc9b92e125dd674aee359
chines05 <gpalmenara@gmail.com>
2025-10-09 18:48:16 -0300

    [NexuApp] Fix: Caso com o token ativo redirecionado para a home

--- NexuApp/app/index.tsx
+++ NexuApp/app/index.tsx
@@ -1,14 +1,23 @@
 import { View, Image, StyleSheet } from 'react-native'
 import { router } from 'expo-router'
 import { useEffect } from 'react'
+import { useAuth } from 'src/context/AuthContext'
 
 export default function IndexPage() {
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      router.replace('/auth/login')
-    }, 2000)
-    return () => clearTimeout(timer)
-  }, [])
+  const { user } = useAuth()
+
+ useEffect(() => {
+  const timer = setTimeout(() => {
+    const getRedirectPath = () => {
+      return user ? '/home' : '/auth/login'
+    }
+
+    router.replace(getRedirectPath())
+  }, 2000)
+
+  return () => clearTimeout(timer)
+}, [user])
+
 
   return (
     <View style={styles.container}>
